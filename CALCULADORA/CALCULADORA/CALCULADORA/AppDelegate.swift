5973a1bb6eb6729987df0d4e136d6571575f6934
Jorge Abalo <jorabalo97@gmail.com>
2023-11-02 10:03:26 +0100

    App calculadora

---
+++ CALCULADORA/CALCULADORA/CALCULADORA/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  CALCULADORA
+//
+//  Created by Jorge Abalo Dieste on 2/11/23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        setupView()
+        return true
+    }
+
+    private func setupView(){
+        
+        window = UIWindow( frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
+    }
+
+
+}
+
---
+++ CALCULADORA/CALCULADORA/CALCULADORA/Controller/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  CALCULADORA
+//
+//  Created by Jorge Abalo Dieste on 2/11/23.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    //Mark: -Initialization
+    
+    init(){
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // Mark: -Lyfe Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
